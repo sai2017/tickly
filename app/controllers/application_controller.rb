a07a118b590670f7a555a1a00fcd1d09f74a559b
佐々木皓晃 <ssk.hrak123@gmail.com>
2020-03-02 12:55:39 +0900

    add user index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up,keys: %i(name self_introduction sex img_name job_category original_experience purpose_of_working weakness want_to_do want_to_connect meet_area))
     devise_parameter_sanitizer.permit(:account_update, keys: %i(name self_introduction sex img_name job_category original_experience purpose_of_working weakness want_to_do want_to_connect meet_area))
   end
+
+  def after_sign_in_path_for(resource)
+    users_path
+  end
 end
