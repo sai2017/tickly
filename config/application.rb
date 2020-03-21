40c4f97d771d087f3192e8c00404d82e1280d4d0
佐々木皓晃 <ssk.hrak123@gmail.com>
2020-03-21 18:11:38 +0900

    push忘れ

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Cordis
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :ja   
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
