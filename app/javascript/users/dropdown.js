e625bc528f66c6cd908e786bbd2fb26da6ba29a5
佐々木皓晃 <ssk.hrak123@gmail.com>
2020-04-03 21:18:55 +0900

    ドロップダウン以外をクリックしてもメニューを閉じる

--- app/javascript/users/dropdown.js
+++ app/javascript/users/dropdown.js
@@ -2,4 +2,11 @@ $(function() {
   $(document).on('click', '.user-edit-category', function() {
     $(this).siblings('.user-edit-list-none').toggle('slow');
   });
+
+  // メニュー領域外をクリックしたらメニューを閉じる
+  $(document).on('click touchend', function(event) {
+    if (!$(event.target).closest('.user-edit-category').length) {
+      $('.user-edit-category').siblings('.user-edit-list-none').slideUp('slow');
+    }
+  });
 });
