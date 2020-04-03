5877e2d057dbccc47a2eea05c5b77f68a5b6774d
佐々木皓晃 <ssk.hrak123@gmail.com>
2020-04-03 22:12:45 +0900

    ログイン後にログイン前のトップページURLにアクセスしたら、ユーザー一覧にリダイレクトさせる

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
+  before_action :logged_in_user
   def index
+  end
+
+  def logged_in_user
+    if current_user.present?
+      redirect_to users_path
+    end
   end
 end
