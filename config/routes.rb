e429c7a8cbf16c5f8dd6cd1f47a7d00825f1fcae
佐々木皓晃 <ssk.hrak123@gmail.com>
2020-04-07 13:37:54 +0900

    新規登録の際にポイント10個をユーザーに付与する

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+  } 
   root to: 'welcome#index'
   resources :messages, only: %i(index create show)
   resources :matching, only: %i(index)
