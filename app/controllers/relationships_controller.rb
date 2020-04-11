fbdcca0f54634ff04d63e40a9b9fb6c00525080c
佐々木皓晃 <ssk.hrak123@gmail.com>
2020-04-12 01:28:58 +0900

    いいねされたユーザーであればポイントを減らすことなくマッチングすることができる

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,9 +2,19 @@ class RelationshipsController < ApplicationController
 
   def create
     @like_point = LikePoint.find_by(user_id: current_user.id)
+    @follower = User.find(params[:following_id])
     if @like_point.balance > 0
-      update_balance = @like_point.balance - 1
+      # いいねされたユーザーであれば、ポイントを減らすことなくマッチングすることができる
+      if current_user.follower?(@follower)
+        update_balance = @like_point.balance
+      else
+        update_balance = @like_point.balance - 1
+      end
       @like_point.update(balance: update_balance)
+      current_user.active_relationships.create(create_params)
+      Relationship.find_user_send_mail(params[:following_id], current_user)
+    # いいねポイントが0でも、いいねされたユーザーであればマッチングすることができる
+    elsif current_user.follower?(@follower)
       current_user.active_relationships.create(create_params)
       Relationship.find_user_send_mail(params[:following_id], current_user)
     else
