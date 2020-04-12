63020ad7c94f4a56a3f8a87959c2bb2ebe987fa4
佐々木皓晃 <ssk.hrak123@gmail.com>
2020-04-12 20:10:01 +0900

    メッセージルームでpopper.jsのエラーが出てたので修正

--- app/javascript/header/popper.js
+++ app/javascript/header/popper.js
@@ -1,14 +1,17 @@
 $(function() {
-  $("[data-toggle=popover]").popover({
-    html : true,
-    trigger: 'manual',
-    content: function() {
-        const content = $(this).attr("data-popover-content");
-        return $(content).children(".popover-body").html();
-    }
-  }).on('click', function () {
-    $(this).popover('toggle');
-  }).on('blur', function () {
-    $(this).popover('hide');
-  });
+  // メッセージルーム画面でpopper.jsを発火させないように、application.html.erbを使用してる画面のみ発火せせる
+  if (window.document.body.id === 'global-body') {
+    $("[data-toggle=popover]").popover({
+      html : true,
+      trigger: 'manual',
+      content: function() {
+          const content = $(this).attr("data-popover-content");
+          return $(content).children(".popover-body").html();
+      }
+    }).on('click', function () {
+      $(this).popover('toggle');
+    }).on('blur', function () {
+      $(this).popover('hide');
+    });  
+  }
 });
