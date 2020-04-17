f2ad5fe8e310d36f8912c25ab7cca0e4921cc456
佐々木皓晃 <ssk.hrak123@gmail.com>
2020-04-17 22:51:52 +0900

    キョウカン履歴に表示させるユーザーを修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,38 @@
 class LikesController < ApplicationController
   def sent
     @current_user = User.find_by(id: current_user.id)
-    @sent_users = @current_user.following
+    all_sent_users = @current_user.following
+    next_sent_users = all_sent_users.map do |sent_user|
+      active_relationship = Relationship.find_by(following_id: sent_user.id, follower_id: current_user.id)
+      passive_relationship = Relationship.find_by(following_id: current_user.id, follower_id: sent_user.id)
+  
+      # 自分からいいねして相手からのいいねがない場合は、自分がいいねしたユーザーを返す
+      if active_relationship.present? && passive_relationship.blank?
+        sent_user
+      # マッチングしていて、自分の方が先にいいねしてた場合は、自分がいいねしたユーザーを返す
+      elsif active_relationship.created_at < passive_relationship.created_at
+        sent_user
+      end
+    end
+    @sent_users = next_sent_users.compact
+
   end
 
   def received
     @current_user = User.find_by(id: current_user.id)
-    @received_users = @current_user.followers
+    all_received_users = @current_user.followers
+    next_received_users = all_received_users.map do |received_user|
+      active_relationship = Relationship.find_by(following_id: received_user.id, follower_id: current_user.id)
+      passive_relationship = Relationship.find_by(following_id: current_user.id, follower_id: received_user.id)
+  
+      # 相手からいいねが来ていて、自分がいいねしてない場合は、いいねしてくれたユーザーを返す
+      if passive_relationship.present? && active_relationship.blank?
+        received_user
+      # マッチングしていて、相手の方が先にいいねしてた場合は、いいねしてくれたユーザーを返す
+      elsif active_relationship.created_at > passive_relationship.created_at
+        received_user
+      end
+    end
+    @received_users = next_received_users.compact
   end
 end
