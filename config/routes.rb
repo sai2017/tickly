c6b4de6b947dbbecc0e32f3784cb00e9fc459dc1
佐々木皓晃 <ssk.hrak123@gmail.com>
2020-04-23 19:39:30 +0900

    メールアドレスの設定画面をsettings/email.html.erbからdevise/registrations/edit.html.erbに移行

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
     :registrations => 'users/registrations',
     :omniauth_callbacks => 'users/omniauth_callbacks'
   } 
+  devise_scope :user do 
+    # メールアドレスの設定画面をsettings/email.html.erbからdevise/registrations/edit.html.erbに移行
+    get 'settings/email' => 'devise/registrations#edit', as: :email_settings
+  end
+
   root to: 'welcome#index'
   resources :messages, only: %i(index create show)
   resources :matching, only: %i(index)
@@ -18,7 +23,8 @@ Rails.application.routes.draw do
   resources :settings, only: %i(index create) do
     collection do
       get :mail_notification
-      get :email
+      # メールアドレスの設定画面をsettings/email.html.erbからdevise/registrations/edit.html.erbに移行したためコメントアウト
+      # get :email
       # Facebookログインのみの実装により、パスワードの変更は現状必要ないのでコメントアウトしておく
       # get :password
     end
