f9ed328fe806e3f21dfddf3dd4290b4a914e2e0d
佐々木皓晃 <ssk.hrak123@gmail.com>
2020-04-27 13:24:41 +0900

    ページネーションを無限スクロールに変更

---
+++ app/javascript/paginate/jscroll.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('.jscroll').jscroll({
+    // 無限に追加する要素は、どこに入れる？
+    contentSelector: '.jscroll', 
+    // 次のページにいくためのリンクの場所は？ ＞aタグの指定
+    nextSelector: 'a.next',
+    // 読み込み中の表示はどうする？
+    loadingHtml: '読み込んでいます'
+  });
+});
