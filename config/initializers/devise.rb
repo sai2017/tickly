b8cd34562b8bde498c28a219b8f31b2c630bb82e
佐々木皓晃 <ssk.hrak123@gmail.com>
2020-05-12 18:39:16 +0900

    本番環境でFacebookログイン出来るように

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -261,7 +261,11 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET_KEY']
+  if Rails.env.production?
+    config.omniauth :facebook, ENV['FACEBOOK_ID_PRODUCTION'], ENV['FACEBOOK_SECRET_KEY_PRODUCTION']
+  else
+    config.omniauth :facebook, ENV['FACEBOOK_ID_DEVELOPMENT'], ENV['FACEBOOK_SECRET_KEY_DEVELOPMENT']
+  end
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
